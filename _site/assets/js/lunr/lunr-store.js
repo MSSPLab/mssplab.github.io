0338a6173cd097d828bf019c4c2f153848b52aac
cocomelonc <cocomelonkz@gmail.com>
2023-05-08 01:54:51 +0600

    add welcome post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Welcome to our cybersecurity blog",
+        "excerpt":"Welcome to our cybersecurity blog! We will describe techniques for the successful work of the red team, the blue team, threat hunting, research and case studies about penetration testing and programming exploits. This blog will include our investigations into cybersecurity incidents and malware analysis. Also, our open source projects will...","categories": ["tutorial"],
+        "tags": ["redteam","blueteam","purpleteam","pentest","malware"],
+        "url": "/tutorial/2023/05/07/welcome.html",
+        "teaser": "/assets/images/1/welcome.png"
+      }]
