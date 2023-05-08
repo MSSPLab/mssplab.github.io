1bb4f4e187102d18824ac467d6792522b16df8a0
cocomelonc <cocomelonkz@gmail.com>
2023-05-08 12:31:48 +0300

    malware analysis report: windealer 2023-05-08

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -4,4 +4,10 @@ var store = [{
         "tags": ["redteam","blueteam","purpleteam","pentest","malware"],
         "url": "/tutorial/2023/05/07/welcome.html",
         "teaser": "/assets/images/1/welcome.png"
+      },{
+        "title": "Malware analysis report: WinDealer (LuoYu Threat Group)",
+        "excerpt":"WinDealer is a type of malware that is used for financial fraud and theft. It is a banking Trojan that is designed to steal sensitive financial information, such as login credentials, credit card numbers, and other personal information from victims’ computers. Following are the capabilities of the malware: Manipulation of...","categories": ["threat-hunting"],
+        "tags": ["blueteam","malware","malwareanalysis"],
+        "url": "/threat-hunting/2023/05/08/malware-analysis-windealer.html",
+        "teaser": "/assets/images/2/2023-05-08_12-08.png"
       }]
